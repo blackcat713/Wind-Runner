60f6dcc6b5acaa919b93bd06a6401a02dbfa9031
blackcat713 <blackkat.1306@gmail.com>
2015-05-03 17:38:09 +0700

    Wind Runner

---
+++ src/game/assets.js
@@ -0,0 +1,14 @@
+game.module(
+    'game.assets'
+)
+.body(function() {
+    
+game.addAsset('05_trees.png');
+game.addAsset('05_bush.png');
+game.addAsset('platform.png');
+game.addAsset('tires.png');
+game.addAsset('player.json');
+game.addAsset('coin.json');
+game.addAsset('oneway.png');
+
+});
---
+++ src/game/config.js
@@ -0,0 +1,9 @@
+pandaConfig = {
+    name: 'WindRunner',
+    version: '0.0.0',
+
+    system: {
+        width: 1920,
+        height: 1024
+    }
+};
